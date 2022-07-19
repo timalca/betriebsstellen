b98cbc7e19b349587b3d2e2d36b9a102d41dd904
Tim Alcantara <tim-alcantara@web.de>
2022-07-19 14:14:23 +0200

    add mock test

--- src/test/java/de/alcantara/betriebsstellen/BetriebsstellenApplicationTests.java
+++ src/test/java/de/alcantara/betriebsstellen/BetriebsstellenApplicationTests.java
@@ -1,13 +1,39 @@
 package de.alcantara.betriebsstellen;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class BetriebsstellenApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void contextLoads() throws Exception {
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/betriebsstelle/XBIM"))//
+				.andExpect(status().isOk())//
+				.andExpect(MockMvcResultMatchers.jsonPath("Name").value("Ingelmunster"))//
+				.andExpect(MockMvcResultMatchers.jsonPath("Kurzname").value("Ingelmunster"))//
+				.andExpect(MockMvcResultMatchers.jsonPath("Typ").value("BF"))//
+				.andReturn();
+
+		mockMvc.perform(MockMvcRequestBuilders.get("/betriebsstelle/KRK"))//
+				.andExpect(status().isOk())//
+				.andExpect(MockMvcResultMatchers.jsonPath("Name").value("Köln-Rodenkirchen"))//
+				.andExpect(MockMvcResultMatchers.jsonPath("Kurzname").value("Rodenkirchen"))//
+				.andExpect(MockMvcResultMatchers.jsonPath("Typ").value("NE-BF"))//
+				.andReturn();
+
 	}
 
 }
